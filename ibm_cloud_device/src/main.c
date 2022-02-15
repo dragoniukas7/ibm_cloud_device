1af07299542b07b427c214fb7f860c0835412a98
dragoniukas7 <andrius.sapitavicius4@gmail.com>
2022-02-15 15:39:11 +0200

    Moved config to seperate function

--- ibm_cloud_device/src/main.c
+++ ibm_cloud_device/src/main.c
@@ -9,29 +9,42 @@
 
 volatile int interrupt = 0;
 
-void sigHandler(int signo) {
+void sigHandler(int signo)
+{
     signal(SIGINT, NULL);
     syslog(LOG_INFO, "Received signal: %d\n", signo);
     interrupt = 1;
 }
 
-void MQTTTraceCallback (int level, char * message){
-    if ( level > 0 )
-        syslog(LOG_DEBUG, "%s\n", message? message:"NULL");
+void MQTTTraceCallback(int level, char *message)
+{
+    if (level > 0)
+        syslog(LOG_DEBUG, "%s\n", message ? message : "NULL");
+}
+
+void set_config(IoTPConfig *config, char *orgId, char *typeId, char *deviceId, char *token)
+{
+
+    IoTPConfig_setProperty(config, "identity.orgId", argv[1]);
+    IoTPConfig_setProperty(config, "identity.typeId", argv[2]);
+    IoTPConfig_setProperty(config, "identity.deviceId", argv[3]);
+    IoTPConfig_setProperty(config, "auth.token", argv[4]);
 }
 
 /* Main program */
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
     int rc = 0;
     struct memory mem;
-    
+
     openlog("ibm_cloud_device", LOG_PID, LOG_USER);
 
     IoTPConfig *config = NULL;
     IoTPDevice *device = NULL;
-    
-    if(argc != 5){
+
+    if (argc != 5)
+    {
         syslog(LOG_ERR, "Wrong argument count\n");
     }
 
@@ -41,56 +54,60 @@ int main(int argc, char *argv[]){
 
     /* Set IoTP Client log handler */
     rc = IoTPConfig_setLogHandler(IoTPLog_FileDescriptor, stdout);
-    if ( rc != 0 ) {
+    if (rc != 0)
+    {
         syslog(LOG_ERR, "Failed to set IoTP Client log handler: rc=%d\n", rc);
-        exit(1);
+        goto end;
     }
-    
+
     /* Create IoTPConfig object using configuration options defined in the configuration file. */
     rc = IoTPConfig_create(&config, NULL);
-    if ( rc != 0 ) {
+    if (rc != 0)
+    {
         syslog(LOG_ERR, "Failed to initialize configuration: rc=%d\n", rc);
-        exit(1);
+        goto end;
     }
-    
-    IoTPConfig_setProperty(config, "identity.orgId", argv[1]);
-    IoTPConfig_setProperty(config, "identity.typeId", argv[2]);
-    IoTPConfig_setProperty(config, "identity.deviceId", argv[3]);
-    IoTPConfig_setProperty(config, "auth.token", argv[4]);
+
+    set_config(config, argv[1], argv[2], argv[3], argv[4]);
 
     /* Create IoTPDevice object */
     rc = IoTPDevice_create(&device, config);
-    if ( rc != 0 ) {
+    if (rc != 0)
+    {
         syslog(LOG_ERR, "Failed to configure IoTP device: rc=%d\n", rc);
-        exit(1);
+        goto end;
     }
 
     /* Set MQTT Trace handler */
     rc = IoTPDevice_setMQTTLogHandler(device, &MQTTTraceCallback);
-    if ( rc != 0 ) {
+    if (rc != 0)
+    {
         syslog(LOG_ERR, "WARN: Failed to set MQTT Trace handler: rc=%d\n", rc);
     }
 
     /* Invoke connection API IoTPDevice_connect() to connect to WIoTP. */
     rc = IoTPDevice_connect(device);
-    if ( rc != 0 ) {
+    if (rc != 0)
+    {
         syslog(LOG_ERR, "Failed to connect to Watson IoT Platform: rc=%d\n", rc);
         syslog(LOG_ERR, "Returned error reason: %s\n", IOTPRC_toString(rc));
-        exit(1);
+        goto end;
     }
 
-    while(!interrupt)
+    while (!interrupt)
     {
-    	get_usage(&mem);
-    	char data[1024]; 
-    	sprintf(data, "{\"totalMemory\": \"%ld\", \"freeMemory\":\"%ld\", \"sharedMemory\":\"%ld\",\"bufferedMemory\":\"%ld\"}",
-    			mem.totalMemory, mem.freeMemory, mem.sharedMemory, mem.bufferedMemory);
-        rc = IoTPDevice_sendEvent(device,"status", &data, "json", QoS0, NULL);
-        
-        if ( rc != 0 ) {
+        get_usage(&mem);
+        char data[1024];
+        sprintf(data, "{\"totalMemory\": \"%ld\", \"freeMemory\":\"%ld\", \"sharedMemory\":\"%ld\",\"bufferedMemory\":\"%ld\"}",
+                mem.totalMemory, mem.freeMemory, mem.sharedMemory, mem.bufferedMemory);
+        rc = IoTPDevice_sendEvent(device, "status", &data, "json", QoS0, NULL);
+
+        if (rc != 0)
+        {
             syslog(LOG_ERR, "Failed to send event: rc=%d\n", rc);
         }
-        else {
+        else
+        {
             syslog(LOG_INFO, "Successfully published data to IBM cloud");
         }
         sleep(10);
@@ -99,11 +116,13 @@ int main(int argc, char *argv[]){
     syslog(LOG_INFO, "Publish event cycle is complete.\n");
 
     rc = IoTPDevice_disconnect(device);
-    if ( rc != IOTPRC_SUCCESS ) {
+    if (rc != IOTPRC_SUCCESS)
+    {
         syslog(LOG_ERR, "Failed to disconnect from  Watson IoT Platform: rc=%d\n", rc);
-        exit(1);
+        goto end;
     }
 
+end:
     IoTPDevice_destroy(device);
     IoTPConfig_clear(config);
     closelog();
